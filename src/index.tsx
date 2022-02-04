73c4565ad6ba531c3cb7093110b4718cfa1b51ed
denya54 <denis-khvesenya@yandex.com>
2022-02-04 19:14:28 +0300

    start commits

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
+//commit1
+
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
