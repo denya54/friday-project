b4f78c6ab9f5788266414fa1830dcad0d9b14232
Kseniya Melnik <kseniyamelnnik@gmail.com>
2022-02-04 22:07:32 +0300

    feat: added markup for the registration page

--- src/ui/Registration/Registration.tsx
+++ src/ui/Registration/Registration.tsx
@@ -1,8 +1,33 @@
+import s from "./Registration.module.css"
+import SuperInputText from "../../SuperComponents/SuperInputText/SuperInputText";
+import SuperButton from "../../SuperComponents/SuperButton/SuperButton";
 
 export const Registration = () => {
+
     return (
         <div>
-            This is page for register
+            <h2>It-incubator</h2>
+            <p>Sign Up</p>
+            <form className={s.form} action="">
+                <div>
+                    <SuperInputText type="email"
+                                    placeholder={"Email"}
+                    />
+                </div>
+                <div>
+                    <SuperInputText type="password"
+                                    placeholder={"password"}
+                    />
+                </div>
+                <div>
+                    <SuperInputText type="password"
+                                    placeholder={"Confirm password"}
+                    />
+                </div>
+                <div>
+                    <SuperButton>Register</SuperButton>
+                </div>
+            </form>
         </div>
     )
 }
