8774a28eec16fee61406db0fc0bbcbe0da13a84b
vladward <vladward13@gmail.com>
2022-02-05 19:08:02 +0300

    feat: add login API

---
+++ src/ui/Login/loginApi/loginAPI.ts
@@ -0,0 +1,37 @@
+import axios, {AxiosResponse} from "axios";
+
+let instance = axios.create({
+    baseURL: 'https://neko-back.herokuapp.com/2.0/'
+})
+
+export const loginAPI = {
+    login(data: LoginParamsType) {
+        return instance.post<LoginParamsType, AxiosResponse<ResponseType<{data: ResponseDataType}>>>('auth/login', data)
+    }
+}
+
+export type LoginParamsType = {
+    email: string
+    password: string
+    rememberMe?: boolean
+}
+
+export type ResponseDataType = {
+    _id: string;
+    email: string;
+    name: string;
+    avatar?: string;
+    publicCardPacksCount: number;
+
+    created: Date;
+    updated: Date;
+    isAdmin: boolean;
+    verified: boolean;
+    rememberMe: boolean;
+
+    error?: string;
+}
+
+export type ResponseType<T = {}> = {
+    data: T
+}
