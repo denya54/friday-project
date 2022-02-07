b0522951742358ab7ab587089693e4ab9d985705
vladward <vladward13@gmail.com>
2022-02-07 21:45:00 +0300

    feat: add ability to send auth cookie

--- src/ui/Login/loginApi/loginAPI.ts
+++ src/ui/Login/loginApi/loginAPI.ts
@@ -1,7 +1,8 @@
 import axios, {AxiosResponse} from "axios";
 
 let instance = axios.create({
-    baseURL: 'https://neko-back.herokuapp.com/2.0/'
+    baseURL: 'https://neko-back.herokuapp.com/2.0/',
+    withCredentials: true
 })
 
 export const loginAPI = {
