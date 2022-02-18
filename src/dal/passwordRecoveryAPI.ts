cad10dd2afdc2f1d4e1f92e213ce59ae80d8e0a5
denya54 <denis-khvesenya@yandex.com>
2022-02-18 17:50:14 +0300

    change baseUrl

--- src/dal/passwordRecoveryAPI.ts
+++ src/dal/passwordRecoveryAPI.ts
@@ -8,8 +8,8 @@ export const ForgotPasswordAPI = {
                 //ПОТОМ ПОМЕНЯТЬ ССЫЛКУ на gitpage.io
                 message: `
 <div style="background-color: lime; padding: 15px">Для создания нового пароля перейдите по ссылке: 
-<!--<a href='https://denya54.github.io/friday-project/#/set_new_password/$token$'>-->
-<a href='http://localhost:3000/set_new_password/$token$'>
+<a href='https://denya54.github.io/friday-project/#/set_new_password/$token$'>
+<!--<a href='http://localhost:3000/set_new_password/$token$'>-->
 link</a></div>
 `,
             })
